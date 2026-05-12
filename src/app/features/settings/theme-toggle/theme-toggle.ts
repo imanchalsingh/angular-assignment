7a3c154c0a38ec248a687f2b9ae464cb5d74f8f8
imanchalsingh <singhanchal159357@gmail.com>
2026-05-12 13:19:08 +0530

    feat : theme toggle

---
+++ src/app/features/settings/theme-toggle/theme-toggle.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-toggle',
+  imports: [],
+  templateUrl: './theme-toggle.html',
+  styleUrl: './theme-toggle.css',
+})
+export class ThemeToggle {}
