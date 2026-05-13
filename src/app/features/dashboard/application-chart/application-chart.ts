a424177d27bcbee13d43877818b17f2ee055aac3
imanchalsingh <singhanchal159357@gmail.com>
2026-05-13 23:50:41 +0530

    added application charts

---
+++ src/app/features/dashboard/application-chart/application-chart.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-chart',
+  imports: [],
+  templateUrl: './application-chart.html',
+  styleUrl: './application-chart.css',
+})
+export class ApplicationChart {}
