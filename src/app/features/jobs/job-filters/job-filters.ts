08cae67e5895a2bf087fbd119f0758d67405d8fc
imanchalsingh <singhanchal159357@gmail.com>
2026-05-14 21:14:06 +0530

    added job filters

---
+++ src/app/features/jobs/job-filters/job-filters.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-filters',
+  imports: [],
+  templateUrl: './job-filters.html',
+  styleUrl: './job-filters.css',
+})
+export class JobFilters {}
