3450a976242d0d43419a3a9c1e02d26428889f7a
imanchalsingh <singhanchal159357@gmail.com>
2026-05-14 21:14:46 +0530

    added job form

---
+++ src/app/features/jobs/job-form/job-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-form',
+  imports: [],
+  templateUrl: './job-form.html',
+  styleUrl: './job-form.css',
+})
+export class JobForm {}
