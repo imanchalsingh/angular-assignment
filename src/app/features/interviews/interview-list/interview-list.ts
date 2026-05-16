62a49edec65b8086663d110086db6d296e8b4d1f
imanchalsingh <singhanchal159357@gmail.com>
2026-05-16 18:47:01 +0530

    added interview list

---
+++ src/app/features/interviews/interview-list/interview-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interview-list',
+  imports: [],
+  templateUrl: './interview-list.html',
+  styleUrl: './interview-list.css',
+})
+export class InterviewList {}
