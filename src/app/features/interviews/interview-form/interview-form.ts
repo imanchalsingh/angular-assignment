e1f04a5cb9e733d8385881cff94cc3f77361250b
imanchalsingh <singhanchal159357@gmail.com>
2026-05-16 18:46:31 +0530

    added interview form

---
+++ src/app/features/interviews/interview-form/interview-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interview-form',
+  imports: [],
+  templateUrl: './interview-form.html',
+  styleUrl: './interview-form.css',
+})
+export class InterviewForm {}
