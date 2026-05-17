bde7eeb24a01bd13a6c9232e295d6900d486fd42
imanchalsingh <singhanchal159357@gmail.com>
2026-05-17 14:50:51 +0530

    added loading spinner

--- src/app/shared/components/loading-spinner/loading-spinner.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loading-spinner',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './loading-spinner.html',
   styleUrl: './loading-spinner.css',
 })
-export class LoadingSpinner {}
+export class LoadingSpinner {
+  @Input() isLoading = false;
+  @Input() message = 'Loading...';
+}
