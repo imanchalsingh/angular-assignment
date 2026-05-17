8070969779f498049e1dfd4e722042646fa558ee
imanchalsingh <singhanchal159357@gmail.com>
2026-05-17 14:18:19 +0530

    feat: add JobCard component with styling, inputs, and delete/edit event emitters

--- src/app/features/jobs/job-card/job-card.ts
+++ src/app/features/jobs/job-card/job-card.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Job } from '../../models/job.model';
 
 @Component({
   selector: 'app-job-card',
-  imports: [],
   templateUrl: './job-card.html',
-  styleUrl: './job-card.css',
+  styleUrls: ['./job-card.css']
 })
-export class JobCard {}
+export class JobCardComponent {
+  @Input() job!: Job;
+
+  @Output() edit = new EventEmitter<string>();
+  @Output() delete = new EventEmitter<string>();
+
+  onEdit(): void {
+    this.edit.emit(this.job.id);
+  }
+
+  onDelete(): void {
+    const confirmed = confirm(
+      `Delete application for ${this.job.position} at ${this.job.company}?`
+    );
+
+    if (confirmed) {
+      this.delete.emit(this.job.id);
+    }
+  }
+}
