39cb8858f5b7267c50f62e7880ffe23d7517dd10
imanchalsingh <singhanchal159357@gmail.com>
2026-05-17 14:50:13 +0530

    added empty stats

--- src/app/shared/components/empty-state/empty-state.ts
+++ src/app/shared/components/empty-state/empty-state.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-empty-state',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './empty-state.html',
   styleUrl: './empty-state.css',
 })
-export class EmptyState {}
+export class EmptyState {
+  @Input() title = 'No Data Found';
+  @Input() message = 'There is nothing to display right now';
+  @Input() icon = '📭';
+}
