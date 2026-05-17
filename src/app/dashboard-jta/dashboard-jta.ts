5c874f001bad8ea40e03d3d2b9c93f0837cfb2ee
imanchalsingh <singhanchal159357@gmail.com>
2026-05-17 14:02:11 +0530

    refactor: remove dashboard-jta components, HTML, CSS, and tests to deprecate module

--- src/app/dashboard-jta/dashboard-jta.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dashboard-jta',
-  imports: [],
-  templateUrl: './dashboard-jta.html',
-  styleUrl: './dashboard-jta.css',
-})
-export class DashboardJta {}
